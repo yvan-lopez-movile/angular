fd0d4041483947b92b1bf95c043dd30ea8807979
Yvan Lopez <yvan.lopez@wavy.global>
2019-09-30 00:54:52 -0500

    assignment 6 complete

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f', {static: false}) signupForm: NgForm;
+  defaultOption = 'Advanced';
+  user = {
+    email: '',
+    subscription: '',
+    password: ''
+  };
+  submitted = false;
 
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.subscription = this.signupForm.value.userData.subscription;
+    this.user.password = this.signupForm.value.userData.password;
+
+    this.signupForm.reset();
+  }
 }
